562e268999b370a06a4dee1e4a3d7a7fcb520c2c
Hutoma <acibelli@hutoma.com>
2016-08-11 23:56:59 +0200

    UI Console - Error Debug PAGE and LOGOUT

--- Front-end/console/dynamic/error.content.html.php
+++ Front-end/console/dynamic/error.content.html.php
@@ -1,68 +1,67 @@
+
 <?php
-$msg ='<div class="row">';
-$msg.='<div class="col-md-12">';
-$msg ="<div class='box box-solid box-danger flat no-shadow'>";
-$msg.="<div class='box-header with-border'>";
-$msg.='<i class="fa fa-warning"></i>';
-$msg.='<h3 class="box-title " >Alerts</h3>';
-$msg.='</div>';
-$msg.='<div class="box-body">';
-$msg.='<div class="alert alert-warning alert-dismissable">';
-$msg.='<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>';
-$msg.='<h4><i class="icon fa fa-ban"></i> Alert!</h4>';
-
-
-
+$msg ='';
 if (isset($_GET['err'])) {
-
     switch ($_GET['err']) {
 
         case 1 :
-            $msg.='SESSION EXIPED';
+            $msg ='SESSION EXIPED';
             break;
         case 2 :
-            $msg.='SESSION AI VARIABILES INCOMPLETE';
+            $msg ='SESSION AI VARIABILES INCOMPLETE';
             break;
         case 3 :
-            $msg.='DOMAINS GET INFO ERROR';
+            $msg ='DOMAINS GET INFO ERROR';
             break;
         case 4 :
-            $msg.='RESPONSE JSON USER DOMAINS ERROR';
+            $msg ='RESPONSE JSON USER DOMAINS ERROR';
             break;
         case 5 :
-            $msg.='RESPONSE JSON CREATE AI ERROR';
+            $msg ='RESPONSE JSON CREATE AI ERROR';
             break;
         case 6 :
-            $msg.='NO CURRENT AI SET';
+            $msg ='NO CURRENT AI SET';
             break;
         case 7 :
-            $msg.='OVERFLOW list of seleted AIs';
+            $msg ='OVERFLOW list of seleted AIs';
             break;
         case 8 :
-            $msg.='JSON list of AIs error';
+            $msg ='JSON list of AIs error';
             break;
         case 9 :
-            $msg.='JSON Delete AI ERROR';
+            $msg ='JSON Delete AI ERROR';
             break;
         case 0 :
-            $msg.='JSON chat ERROR';
+            $msg ='JSON chat ERROR';
             break;
         case 10 :
-            $msg.='aiid SESSION MISSING';
+            $msg ='aiid SESSION MISSING';
             break;
         case 11 :
-            $msg.='training FILE ERROR';
+            $msg ='training FILE ERROR';
             break;
         default:
-            $msg.='UNDEFINED ERROR';
+            $msg ='UNDEFINED ERROR';
     }
-
 }
 else
-    $msg.='UNESPECTED ERROR OCCURED';
-
-$msg.='</div>';
-$msg.='</div>';
-$msg.='</div>';
-echo $msg;
+    $msg ='UNESPECTED ERROR OCCURED';
 ?>
+
+<div class="row">
+    <div class="col-md-12">
+        <div class='box box-solid box-clean flat no-shadow'>
+            <div class='box-header with-border'>
+                <i class="fa fa-warning text-danger"></i>
+                <h3 class="box-title " >Alerts</h3>
+            </div>
+            <div class="box-body">
+                <div class="alert alert-warning alert-dismissable">
+                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                    <h4><i class="icon fa fa-ban"></i> Alert!</h4>
+                    <?php echo $msg; unset($msg); ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
