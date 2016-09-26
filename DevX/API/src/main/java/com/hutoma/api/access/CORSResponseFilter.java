30efc5c7273a033e4c43cfeea84cffb971ed5887
maurizio cibelli <mcibelli@hotmail.com>
2016-09-26 08:47:39 +0200

    adding custom filter to let cors call trough

---
+++ DevX/API/src/main/java/com/hutoma/api/access/CORSResponseFilter.java
@@ -0,0 +1,24 @@
+package com.hutoma.api.access;
+
+/**
+ * Created by mauriziocibelli on 25/09/16.
+ */
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.ext.Provider;
+import java.io.IOException;
+
+@Provider
+public class CORSResponseFilter implements ContainerResponseFilter {
+
+    @Override
+    public void filter(ContainerRequestContext request, ContainerResponseContext response) throws IOException {
+        response.getHeaders().add("Access-Control-Allow-Origin", "*");
+        response.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
+        response.getHeaders().add("Access-Control-Allow-Credentials", "true");
+        response.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+    }
+
+}
