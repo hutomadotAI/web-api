0903de547b0471e9d5291ee9836d51718a8c4262
David MG <mont@dixiak.com>
2016-10-06 10:42:47 +0100

    Revert "Merge remote-tracking branch 'origin/master' into maurizio/missing_logo_fix"
    This reverts commit e91c470d1675e43de3eba5a48e6f4f925302e525, reversing
    changes made to 5743cec9cb2a4ed8aa7c9d84dd45c7a2e67377b7.

---
+++ Front-end/console/dashboard/chat.html.php
@@ -0,0 +1,68 @@
+<div class="box box-solid box-clean flat no-shadow direct-chat direct-chat-success">
+    <div class="box-header with-border">
+        <i class="fa fa-comment-o"></i>
+        <h3 class="box-title"><?php echo $_SESSION[ $_SESSION['navigation_id'] ]['user_details']['ai']['name']; ?> - Chat</h3>
+
+
+        <div class="box-tools pull-right" >
+            <ul class="dropdown-menu flat">
+                <li class="footer"><a href="#">  <i class="fa fa-bullhorn"></i>Deactive Voice</a></li>
+                <li class="footer"><a href="#">  <i class="fa fa-microphone-slash"></i>Mute Microphone</a></li>
+                <li class="footer"><a href="#">  <i class="fa fa-adjust"></i>Color Voice</a></li>
+                <li class="footer" id="continuous-option" value ="0" onClick="continuousOption(this.value)" onMouseOver="this.style.cursor='pointer'">
+                    <a id="continuous-type" ><i id="continuous-icon" class="fa fa-retweet"></i><spam id="continuous-text">Continuous Speech</spam></a>
+                </li>
+            </ul>
+
+
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" data-toggle="tooltip" title="voice options" tabindex="-1" >
+                <i class="fa fa-gears"></i>
+            </a>
+         
+            <button class="btn btn-box-tool" data-widget="collapse" tabindex="-1" ><i class="fa fa-minus"></i></button>
+        </div>
+
+    </div>
+          
+    <div class="box-body">
+        <div class="direct-chat-messages" id="chat">
+
+        </div>
+    </div>
+
+    <div class="box-footer" id="chat-footer">
+
+
+        <div class="input-group">
+            <input type="text" id="message" placeholder="Type a message ..." class="form-control" onkeydown="keyboardChat(event)"  tabindex="-1">
+
+            <div class="input-group-addon" id="btnSpeech" onMouseOver="this.style.cursor='pointer'">
+                <i id="microphone" style="font-size: 18px;" class="fa fa-microphone text-red"></i>
+            </div>
+        </div>
+
+
+    </div>
+</div>
+
+
+
+
+
+<!--
+<script src="./plugins/chat/chat.drawing.html.js"></script>
+
+<form action="" method="post" enctype="multipart/form-data">
+    <script type="text/javascript">
+        drawHTML.init(["<?php //echo $_SESSION[$_SESSION['navigation_id']]['user_details']['username']; ?>","<?php echo $_SESSION[ $_SESSION['navigation_id'] ]['user_details']['ai']['name']; ?>"]);
+    </script>
+</form>
+-->
+
+
+
+
+
+
+
+  
