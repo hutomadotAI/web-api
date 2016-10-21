7b5a5ed2317a5dac7699ebf0a2f05f82d042022b
mcibelli <mcibelli@hutoma.com>
2016-10-21 15:48:45 +0000

    PR 151: making our helper dialog less intrusive
    toning down colors and remove the "remove me" option

--- Front-end/console/dynamic/domainsNewAI.content.info.html.php
+++ Front-end/console/dynamic/domainsNewAI.content.info.html.php
@@ -1,18 +1,9 @@
-<div class="alert alert-dismissable flat alert-primary" id="containerMsgAlertDomainsNewAIInfo" style="padding-bottom: 25px;">
+<div class="alert alert-dismissable flat alert-warning" id="containerMsgAlertDomainsNewAIInfo" style="padding-bottom: 25px;">
     <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-    <span id="msgAlertDomainsNewAIInfo">
-        <dt>Add Pre-trained neural networks bla bla bla</dt>
+    <span id="msgAlertDomainsNewAIInfo"  class="text-muted">
+        <dt>What is a pre-trained Neural NetworK?</dt>
         <dd>
             bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla
-            bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla
-            bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla
-            bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla
         </dd>
-        <div class="pull-right" style="padding-right: 3px;">
-        <label for="ai_public">
-            <input type="checkbox" name="dialog_msg_domains" id="dialog_msg_domains">
-        </label>
-            do not show this message again
-        </div>
-    </span>
+        </span>
 </div>
--- Front-end/console/dynamic/training.content.info.html.php
+++ Front-end/console/dynamic/training.content.info.html.php
@@ -1,19 +1,9 @@
-<div class="alert alert-dismissable flat alert-primary" id="containerMsgAlertTrainingInfo" style="padding-bottom: 25px;">
+<div class="alert alert-dismissable flat alert-warning" id="containerMsgAlertTrainingInfo" style="padding-bottom: 25px;">
     <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-    <span id="msgAlertTrainingInfo">
-        <dt>Start info pre-trained neural Manipulation</dt>
+    <span id="msgAlertTrainingInfo" class="text-muted" >
+        <dt>How do you train an AI?</dt>
         <dd>
-            The goals of the training program should relate directly to the needs determined by the assessment process outlined above.
-            Course objectives should clearly state what behavior or skill will be changed as a result of the training and should relate
-            to the mission and strategic plan of the company. Goals should include milestones to help take the employee from where he or
-            she is today to where the firm wants him or her in the future. Setting goals helps to evaluate the training program and also
-            to motivate employees. Allowing employees to participate in setting goals increases the probability of success.
+            Add some stuff here about training.
         </dd>
-        <div class="pull-right" style="padding-right: 3px;">
-        <label for="ai_public">
-            <input type="checkbox" name="dialog_msg" id="dialog_msg">
-        </label>
-            do not show this message again
-        </div>
     </span>
 </div>
