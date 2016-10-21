849d5de7db29522aca1d9b0ab9bd908fa526004c
mcibelli <mcibelli@hutoma.com>
2016-10-21 11:09:39 +0000

    PR 146: disabling profile UI from the nav bar until we have an actual profile UI
     - disabling profile UI from the nav bar until we have an actual profile UI

--- Front-end/console/dynamic/navigationbar.account.html.php
+++ Front-end/console/dynamic/navigationbar.account.html.php
@@ -27,24 +27,26 @@
         </li>
         -->
 
-        <!-- User Account: style can be found in dropdown.less -->
+        <!-- User Account: style can be found in dropdown.less
+
+
         <li class="dropdown user user-menu">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" tabindex="-1" >
             <i class="fa fa-cog text-gray" tabindex="-1"></i><span style="font-size:10px;color:gray;">MY ACCOUNT</span>
           </a>
 
-          <ul class="dropdown-menu">
-            <!-- User image -->
+
+
+           <ul class="dropdown-menu">
             <li class="user-header">
               <img src="./dist/img/user1-160x160.jpg" class="img-circle" alt="User Image">
               <p>
-                  <?php  echo ( $username.' - planID '. $plan);?>
+                  <?php  // echo ( $username.' - planID '. $plan);?>
                 <small>
-                  <?php echo 'joined since ',$joined ;?>
+                  <?php // echo 'joined since ',$joined ;?>
                 </small>
               </p>
             </li>
-            <!-- Menu Footer-->
             <li class="user-footer">
               <div class="pull-left">
                 <a href="#" class="btn btn-default btn-flat" tabindex="-1">Profile</a>
@@ -54,7 +56,10 @@
               </div>
             </li>
           </ul>
+
+
         </li>
+  -->
       </ul>
 </div>
 <?php unset($username); unset($plan); unset($joined); ?>
