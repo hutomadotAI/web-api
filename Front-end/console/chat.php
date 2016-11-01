97a9a595e3ba71b427326a51000cce75fbb55ea9
David Montanaro Gauci <david.montanaro@hutoma.com>
2016-11-01 16:27:28 +0000

    PR 228: fix to console chat code when API sends a null topic_out
     - check whether the array_key_exists before setting history or topic_out
    
    Related work items: #490

--- Front-end/console/chat.php
+++ Front-end/console/chat.php
@@ -1,56 +1,58 @@
 <?php
 require "../pages/config.php";
 
-    if((!\hutoma\console::$loggedIn)||(!\hutoma\console::isSessionActive())) {
-        \hutoma\console::redirect('../pages/login.php');
-        exit;
-    }
+if ((!\hutoma\console::$loggedIn) || (!\hutoma\console::isSessionActive())) {
+    \hutoma\console::redirect('../pages/login.php');
+    exit;
+}
 
 
-    if ( !isset($_SESSION[ $_SESSION['navigation_id'] ]['user_details']['ai']['aiid']) ){
-        header('Location: ./error.php?err=2');
-        exit;
-    }
+if (!isset($_SESSION[$_SESSION['navigation_id']]['user_details']['ai']['aiid'])) {
+    header('Location: ./error.php?err=2');
+    exit;
+}
 
-    $hist="";
-    if(isset($_SESSION[ $_SESSION['navigation_id'] ]['user_details']['ai']['history'])) {
-        $hist = $_SESSION[ $_SESSION['navigation_id'] ]['user_details']['ai']['history'];
-    }
+$hist = "";
+if (isset($_SESSION[$_SESSION['navigation_id']]['user_details']['ai']['history'])) {
+    $hist = $_SESSION[$_SESSION['navigation_id']]['user_details']['ai']['history'];
+}
 
-    $confidence="0.0";
-    if(isset($_SESSION[ $_SESSION['navigation_id'] ]['user_details']['ai']['confidence'])) {
-        $confidence = $_SESSION[ $_SESSION['navigation_id'] ]['user_details']['ai']['confidence'];
-    }
+$confidence = "0.0";
+if (isset($_SESSION[$_SESSION['navigation_id']]['user_details']['ai']['confidence'])) {
+    $confidence = $_SESSION[$_SESSION['navigation_id']]['user_details']['ai']['confidence'];
+}
 
-    $topic="";
-    if(isset($_SESSION[ $_SESSION['navigation_id'] ]['user_details']['ai']['topic'])) {
-        $topic = $_SESSION[ $_SESSION['navigation_id'] ]['user_details']['ai']['topic'];
-    }
+$topic = "";
+if (isset($_SESSION[$_SESSION['navigation_id']]['user_details']['ai']['topic'])) {
+    $topic = $_SESSION[$_SESSION['navigation_id']]['user_details']['ai']['topic'];
+}
 
-    $response = \hutoma\console::chatAI(
-        $_SESSION[ $_SESSION['navigation_id'] ]['user_details']['ai']['aiid'], // aiid
-        $_GET['chatId'], // chatId
-        $_GET['q'], // question
-        $hist, // history
-        false, // fs
-        $confidence, // min_p
-        $topic);
+$response = \hutoma\console::chatAI(
+    $_SESSION[$_SESSION['navigation_id']]['user_details']['ai']['aiid'], // aiid
+    $_GET['chatId'], // chatId
+    $_GET['q'], // question
+    $hist, // history
+    false, // fs
+    $confidence, // min_p
+    $topic);
 
-    if ($response['status']['code'] !== 200) {
-        echo(json_encode($response,JSON_PRETTY_PRINT));
-        unset($response);
-        exit;
-    }
-
-    $_SESSION[$_SESSION['navigation_id']]['user_details']['ai']['history'] = '';
-    $_SESSION[$_SESSION['navigation_id']]['user_details']['ai']['topic'] = '';
-
-    if (isset($response) && array_key_exists('result', $response)) {
-        $_SESSION[$_SESSION['navigation_id']]['user_details']['ai']['history'] = $response['result']['history'];
-        $_SESSION[$_SESSION['navigation_id']]['user_details']['ai']['topic'] = $response['result']['topic_out'];
-    }
-
-    echo json_encode($response,JSON_PRETTY_PRINT);
+if ($response['status']['code'] !== 200) {
+    echo(json_encode($response, JSON_PRETTY_PRINT));
     unset($response);
+    exit;
+}
+
+$_SESSION[$_SESSION['navigation_id']]['user_details']['ai']['history'] = '';
+$_SESSION[$_SESSION['navigation_id']]['user_details']['ai']['topic'] = '';
+
+if (isset($response) && array_key_exists('result', $response)) {
+    $_SESSION[$_SESSION['navigation_id']]['user_details']['ai']['history']
+        = array_key_exists('history', $response['result']) ? $response['result']['history'] : '';
+    $_SESSION[$_SESSION['navigation_id']]['user_details']['ai']['topic']
+        = array_key_exists('topic_out', $response['result']) ? $response['result']['topic_out'] : '';
+}
+
+echo json_encode($response, JSON_PRETTY_PRINT);
+unset($response);
 ?>
 
