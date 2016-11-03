415f0ff5ca2196a9b1d58d6219a37aa85c2bcc3c
Hutoma <acibelli@hutoma.com>
2016-11-03 17:40:30 +0100

    UI - Boxes store - change little parts of box show

--- Front-end/console/dynamic/botstore.content.info.details.html.php
+++ Front-end/console/dynamic/botstore.content.info.details.html.php
@@ -9,24 +9,26 @@
     <div class="modal-dialog flat">
         <!-- Modal Prompt content-->
         <div class="modal-content padding" style="background-color: #202020">
+
             <div class="modal-header">
-                <button type="button" class="close" id="btnModelClose" data-dismiss="modal">&times;</button>
-                <h4 class="modal-title"><i class="fa fa-tag text-md text-white" style="padding-right:2em"></i>
+                <button type="button" class="close text-gray" id="btnModelClose" data-dismiss="modal">&times;</button>
+                <div class="modal-title">
                     <span name="curr_bot_name" value=""></span>
-                </h4>
+                </div>
+                <div class="box-header with-border">
+                    <h3 class="box-title" id="curr_bot_descritpion"></h3>
+                </div>
             </div>
 
-            <div class="box-body no-margin" style="padding-top: 0;">
+            <div class="modal-body" style="background-color: #202020">
                 <div class="row">
                     <div class="col-md-12">
-                        <div class="box box-solid">
-                            <div class="box-header with-border">
-                                <h3 class="box-title" id="curr_bot_descritpion"></h3>
-                            </div>
+                        <div class="box box-solid flat no-padding">
                             <div class="box-body">
                                 <dl>
                                     <dt>More details</dt>
                                     <dd id="curr_bot_details"></dd>
+                                    <br>
                                     <dt>Uses case</dt>
                                     <df id="curr_bot_usecase"></df>
                                 </dl>
@@ -37,7 +39,7 @@
             </div>
             
             <div class="modal-footer">
-                    <button type="button" class="btn btn-primary flat" id="btnModelDomainInfo" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary flat" id="btnModelDomainInfo" data-dismiss="modal">Close</button>
             </div>
 
 
