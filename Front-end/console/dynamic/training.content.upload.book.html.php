2104d265c495822459170a1eb9d02f24b895c1a2
Hutoma <acibelli@hutoma.com>
2016-11-03 18:05:20 +0100

    UI - Info box upload text adjust

--- Front-end/console/dynamic/training.content.upload.book.html.php
+++ Front-end/console/dynamic/training.content.upload.book.html.php
@@ -42,19 +42,69 @@
 
 
 <!-- MODAL Example training file -->
+<div class="modal fade" id="sampleTrainingFile" role="dialog">
+    <div class="modal-dialog flat">
+
+        <!-- Modal content-->
+        <div class="modal-content bordered" style="background-color: #212121">
+            <div class="modal-header  ">
+                <button type="button" class="close text-gray" id="btnModelClose" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">   <i class="fa fa-file-text-o text-success" style="padding-right:2em"></i> Training file sample</h4>
+            </div>
+            <div class="modal-body">
+                <div class="box-body" id="example-training-file">
+                    <span id="contentSampleFile"></span>
+                    <dt>hello my phone is broken</dt>
+                    <dt>oh sorry what is the problem</dt>
+                    <dt>the screen does not turn on</dt>
+                    <dt>hmmm ok. have you tried charging</dt>
+                    <dt>it is charged i think</dt>
+                    <dt>ok what happens when you press the power button</dt>
+                    <dt>nothing happens</dt>
+                    <dt>ok let me send you someone to pick up your phone</dt>
+                    </br>
+                    <dt>hello</dt>
+                    <dt>hi, how are you</dt>
+                    <dt>i am fine thanks! and you?</dt>
+                    <dt>I am good</dt>
+                    </br>
+                    <dt>what is your name</dt>
+                    <dt>my name is AI1</dt>
+                    </br>
+                    <dt>what does your name mean?</dt>
+                    <dt>not sure. ask maurizio</dt>
+                    </br>
+                    <dt>waht is the meaning of life</dt>
+                    <dt>the meaning of life is 42</dt>
+                    <?php //echo file_get_contents('./dist/file/sampleTrainingFile.txt');?>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="submit" class="btn btn-primary flat" id="btnModalUploadSample" data-dismiss="modal">Upload</button>
+                <button type="button" class="btn btn-primary flat" id="btnModalCancelSample" data-dismiss="modal">Cancel</button>
+            </div>
+        </div>
+
+    </div>
+</div>
+
+
+
+
+<!-- MODAL Example training file -->
 <div class="modal fade" id="sampleTrainingBook" role="dialog">
     <div class="modal-dialog flat">
 
         <!-- Modal content-->
-        <div class="modal-content bordered">
+        <div class="modal-content bordered" style="background-color: #212121">
             <div class="modal-header">
-                <button type="button" class="close" id="btnModelClose" data-dismiss="modal">&times;</button>
+                <button type="button" class="close text-gray" id="btnModelClose" data-dismiss="modal">&times;</button>
                 <h4 class="modal-title">   <i class="fa fa-file-text-o text-success" style="padding-right:2em"></i> Training book file sample</h4>
             </div>
             <div class="modal-body">
                 <div class="box-body" id="example-training-book">
                     <span id="contentSampleFile"></span>
-                    <?php echo file_get_contents('./dist/file/sampleTrainingBook.txt');?>
+                    <?php //echo file_get_contents('./dist/file/sampleTrainingBook.txt');?>
                 </div>
             </div>
             <div class="modal-footer">
