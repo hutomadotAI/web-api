0283e2a00c084321dd374a771dcfc4d30f0469bb
Pedro Teixeira <pedrotei@hutoma.com>
2016-11-05 21:05:02 +0000

    PR 241: Fix delete ai page not being loaded correctly
     - Fix delete ai page not being loaded correctly.
    Nevermind the whitespacing and re-formatting, the fix is on line#153
    
    Related work items: #519

--- Front-end/console/dynamic/settings.content.general.html.php
+++ Front-end/console/dynamic/settings.content.general.html.php
@@ -1,7 +1,7 @@
 <div class="box-header with-border">
     <i class="fa fa-sliders text-success"></i>
     <div class="box-title"><b>General Informations</b></div>
-    <a data-toggle="collapse"  href="#collapseInfoGeneral">
+    <a data-toggle="collapse" href="#collapseInfoGeneral">
         <div class=" pull-right">more info
             <i class="fa fa-question-circle text-sm text-yellow"></i>
         </div>
@@ -11,7 +11,7 @@
 <div id="collapseInfoGeneral" class="panel-collapse collapse">
     <div class="box-body">
         <div class="overlay center-block">
-            <section class="content-info" >
+            <section class="content-info">
                 <div class="box-body">
                     <dl class="dl-horizontal">
                         <dt>Description General aspectog AI</dt>
@@ -22,7 +22,9 @@
                         <dt>Malesuada porta</dt>
                         <dd>Etiam porta sem malesuada magna mollis euismod.</dd>
                         <dt>Felis euismod semper eget lacinia</dt>
-                        <dd>Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</dd>
+                        <dd>Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa
+                            justo sit amet risus.
+                        </dd>
                     </dl>
                 </div>
             </section>
@@ -76,16 +78,24 @@
             <div class="col-md-6">
                 <div class="input-group" style="padding-bottom:10px;">
                     <span class="input-group-addon text-gray" style="width:90px;">AI ID</i></span>
-                    <input type="text" class="form-control flat no-shadow" id="aikey" value="<?php echo $_SESSION[ $_SESSION['navigation_id'] ]['user_details']['ai']['aiid'];?>" disabled>
-                    <span class="input-group-addon text-gray" data-clipboard-action="copy" data-toggle="tooltip"  data-clipboard-target="#aikey" id="aikeytooltip" title="copy to clipboard" onclick="copyToClipboard('aikey')" ><i class="fa fa-clipboard"></i></span>
+                    <input type="text" class="form-control flat no-shadow" id="aikey"
+                           value="<?php echo $_SESSION[$_SESSION['navigation_id']]['user_details']['ai']['aiid']; ?>"
+                           disabled>
+                    <span class="input-group-addon text-gray" data-clipboard-action="copy" data-toggle="tooltip"
+                          data-clipboard-target="#aikey" id="aikeytooltip" title="copy to clipboard"
+                          onclick="copyToClipboard('aikey')"><i class="fa fa-clipboard"></i></span>
                 </div>
             </div>
 
             <div class="col-md-6">
                 <div class="input-group" style="padding-bottom:10px;">
                     <span class="input-group-addon text-gray" style="width:90px;">Dev key</i></span>
-                    <input type="text" class="form-control flat no-shadow" id="devkey" value="<?php echo $_SESSION[ $_SESSION['navigation_id'] ]['user_details']['dev_token'];?>" disabled>
-                    <span class="input-group-addon text-gray" data-clipboard-action="copy" data-toggle="tooltip"  data-clipboard-target="#devkey" id="devkeytooltip" title="copy to clipboard" onclick="copyToClipboard('devkey')"><i class="fa fa-clipboard"></i></span>
+                    <input type="text" class="form-control flat no-shadow" id="devkey"
+                           value="<?php echo $_SESSION[$_SESSION['navigation_id']]['user_details']['dev_token']; ?>"
+                           disabled>
+                    <span class="input-group-addon text-gray" data-clipboard-action="copy" data-toggle="tooltip"
+                          data-clipboard-target="#devkey" id="devkeytooltip" title="copy to clipboard"
+                          onclick="copyToClipboard('devkey')"><i class="fa fa-clipboard"></i></span>
                 </div>
             </div>
         </div>
@@ -94,8 +104,12 @@
             <div class="col-md-12">
                 <div class="input-group">
                     <span class="input-group-addon text-gray" style="width:90px;">Client key</i></span>
-                    <input type="text" class="form-control flat no-shadow" id="clikey" value="<?php echo $_SESSION[ $_SESSION['navigation_id'] ]['user_details']['ai']['client_token'];?>" disabled>
-                    <span class="input-group-addon text-gray" data-clipboard-action="copy" data-toggle="tooltip"  data-clipboard-target="#clikey" id="clikeytooltip" title="copy to clipboard" onclick="copyToClipboard('clikey')"><i class="fa fa-clipboard"></i></span>
+                    <input type="text" class="form-control flat no-shadow" id="clikey"
+                           value="<?php echo $_SESSION[$_SESSION['navigation_id']]['user_details']['ai']['client_token']; ?>"
+                           disabled>
+                    <span class="input-group-addon text-gray" data-clipboard-action="copy" data-toggle="tooltip"
+                          data-clipboard-target="#clikey" id="clikeytooltip" title="copy to clipboard"
+                          onclick="copyToClipboard('clikey')"><i class="fa fa-clipboard"></i></span>
                 </div>
             </div>
         </div>
@@ -110,13 +124,14 @@
 </form>
 
 
-
 <div class="box-footer">
-    <button  name="btnCancel" id="btnReset" value="_cancel"   class="btn btn-primary flat" >reset</button>
-    <button  name="btnSave"   id="btnSave"   value="_save"   class="btn btn-success flat" >save</button>
-    <button  name="btnDelete" id="btnDelete"  data-toggle="modal" data-target="#deleteAI" value="<?php echo $_SESSION[ $_SESSION['navigation_id'] ]['user_details']['ai']['name']; ?>" class="btn btn-danger flat pull-right" alt="delete">delete AI</button>
+    <button name="btnCancel" id="btnReset" value="_cancel" class="btn btn-primary flat">reset</button>
+    <button name="btnSave" id="btnSave" value="_save" class="btn btn-success flat">save</button>
+    <button name="btnDelete" id="btnDelete" data-toggle="modal" data-target="#deleteAI"
+            value="<?php echo $_SESSION[$_SESSION['navigation_id']]['user_details']['ai']['name']; ?>"
+            class="btn btn-danger flat pull-right" alt="delete">delete AI
+    </button>
 </div>
-
 
 
 <!-- Modal DELETE AI-->
@@ -126,7 +141,8 @@
         <div class="modal-content bordered" style="background-color: #202020">
             <div class="modal-header">
                 <button type="button" class="close" id="btnModelClose" data-dismiss="modal">&times;</button>
-                <h4 class="modal-title">   <i class="fa fa fa-warning text-danger" style="padding-right:2em"></i> DELETE AI</h4>
+                <h4 class="modal-title"><i class="fa fa fa-warning text-danger" style="padding-right:2em"></i> DELETE AI
+                </h4>
             </div>
             <div class="modal-body">
                 <div class="box-body" id="delete-ai-label">
@@ -134,9 +150,11 @@
                 </div>
             </div>
             <div class="modal-footer">
-                <form method="POST" id="deleteForm" action="./dynamic/deleteAI.php">
-                    <button type="button" class="btn btn-primary flat" id="btnModelCancel" data-dismiss="modal">Cancel</button>
-                    <button type="submit" class="btn btn-danger flat" id="modalDelete" data-dismiss="modal">Delete</button>
+                <form method="POST" id="deleteForm" action="./dynamic/deleteai.php">
+                    <button type="button" class="btn btn-primary flat" id="btnModelCancel" data-dismiss="modal">Cancel
+                    </button>
+                    <button type="submit" class="btn btn-danger flat" id="modalDelete" data-dismiss="modal">Delete
+                    </button>
                 </form>
             </div>
         </div>
