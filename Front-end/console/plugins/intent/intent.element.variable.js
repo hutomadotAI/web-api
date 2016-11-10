2122e031414e62bc042948b9c49b59c4a92abdf9
Hutoma <acibelli@hutoma.com>
2016-11-10 18:19:48 +0000

    UI - Intent Prompt - responses block same name

--- Front-end/console/plugins/intent/intent.element.variable.js
+++ Front-end/console/plugins/intent/intent.element.variable.js
@@ -1,5 +1,46 @@
 document.getElementById("addParameter").addEventListener("click", addEmptyVariableRow);
 
+function loadVariablesFromIntent() {
+    if (typeof intent['variables'] == "undefined" || !(intent['variables'] instanceof Array))
+        return;
+
+    var list_variables = intent['variables'];
+    var intent_name = intent['intent_name'];
+
+    for (var x in list_variables) {
+        var entity = '@' + list_variables[x].entity_name;
+        var n_prompts = list_variables[x].n_prompts;
+        var value = list_variables[x].value;
+        var required = list_variables[x].required;
+        var prompts = list_variables[x].prompts;
+        var parent = document.getElementById('parameter-list');
+        var len = list_variables[x].prompts.length;
+        createNewParameterRow(entity, intent_name, n_prompts, prompts, len, value, required, parent);
+    }
+}
+
+function loadEntities() {
+    var array = [];
+    for (var x in entityListFromServer) {
+        array.push('@' + entityListFromServer[x]);
+    }
+    var $input = $('#action-entity');
+
+    $input.omniselect({
+        source: array,
+        resultsClass: 'typeahead dropdown-menu flat no-padding no-border',
+        activeClass: 'active',
+        renderItem: function (label, id, index) {
+            return '<li><a href="#">' + label + '</a></li>';
+        }
+    });
+
+    $input.on('omniselect:select', function (event, value) {
+        console.log('Selected: ' + value);
+    });
+
+}
+
 function createNewParameterRow(entity, intent_name, n_prompts, prompts, size, value, required, parent) {
 
     if (isJustAddedNewRow())
@@ -16,7 +57,7 @@ function createNewParameterRow(entity, intent_name, n_prompts, prompts, size, va
     wHTML += ('<div class="text-center" >');
 
     if (typeof(entity) === 'undefined' || (entity) == '')
-        wHTML += ('<input type="text" class="form-control flat no-shadow no-border" name="action-entity"  id="action-entity" style="background-color: transparent; margin:0;" placeholder="add entity" autocomplete="off" >');
+        wHTML += ('<input type="text" class="form-control flat no-shadow no-border" name="action-entity"  id="action-entity" style="background-color: transparent; margin:0;" placeholder="add entity" autocomplete="off">');
     else
         wHTML += ('<input type="text" class="form-control flat no-shadow no-border" name="action-entity"  id="action-entity" style="background-color: transparent; margin:0;" placeholder="' + entity + '" autocomplete="off" disabled="disabled">');
 
@@ -31,7 +72,6 @@ function createNewParameterRow(entity, intent_name, n_prompts, prompts, size, va
 
     wHTML += ('<div class="col-xs-4">');
     wHTML += ('<div class="text-center" >');
-
 
     if (size > 0)
         wHTML += ('<input type="text" class="form-control flat no-shadow no-border text-center" id="action-prompts" name="action-prompts" style="background-color: transparent; margin:0;"' +
@@ -99,6 +139,12 @@ function createNewParameterRow(entity, intent_name, n_prompts, prompts, size, va
         }
     });
 
+
+    // TODO need to dropdown menu on click on this event i do not
+    $(inputNode).on('click', function( event, ui ) {
+        //$(this).trigger(jQuery.Event("keydown"));
+    });
+
     // after selection remove value on input text and set placeholder
     $(inputNode).on('omniselect:select', function (event, value) {
         $(inputNode).val('');
@@ -134,4 +180,96 @@ function resetMsgAlertIntentVariable() {
     containerMsgAlertIntentVariable(0, 'Set the parameters for the intents using existing entities.');
 }
 
+function isUsedEntities(entity_name) {
+    var parent = document.getElementById('parameter-list');
+    //  id NODE - relative at entities are in variables list under entity field
+    var len = parent.childElementCount;
+
+    while (--len) {
+        var node = parent.children[len].children[0].children[0].children[0];
+        if (node.placeholder.replace(/[@]/g, "") == entity_name)
+            return true;
+    }
+    return false;
+}
+
+function releaseUsedEntities() {
+
+    var parent = document.getElementById('parameter-list');
+    if (!parent.hasChildNodes())
+        return;
+
+    var node = parent.children[0].children[0].children[0].children[0];
+    var array = [];
+
+    if (node.placeholder != 'add entity')
+        return;
+
+    // loading stored entities
+    for (var x in entityListFromServer) {
+        if (!isUsedEntities(entityListFromServer[x]))
+            array.push('@' + entityListFromServer[x]);
+    }
+
+    $(node).omniselect({
+        source: array,
+        resultsClass: 'typeahead dropdown-menu flat no-padding no-border',
+        activeClass: 'active',
+        renderItem: function (label, id, index) {
+            return '<li><a href="#">' + label + '</a></li>';
+        }
+    });
+
+}
+
+function isJustAddedNewRow() {
+    var parent = document.getElementById('parameter-list');
+    // if parameter list is empty then you can ADD new empty row
+    if (!parent.hasChildNodes())
+        return false;
+
+    // if entity field value is default value it means you just add a new row
+    var node = parent.children[0].children[0].children[0].children[0];
+    if (node.placeholder == 'add entity')  // se hai raggiunto in numero massimo MSG ALERT
+        return true;
+
+    var len = parent.childElementCount;
+    if (len == entityListFromServer.length) { // se hai raggiunto in numero massimo MSG ALERT
+        containerMsgAlertIntentVariable(1, 'All entities are used!');
+        return true;
+    }
+
+    return false;
+}
+
+
+function loadPromptsForEntity(curr_entity) {
+    if (typeof intent['variables'] == "undefined" || !(intent['variables'] instanceof Array))
+        return;
+
+    var list_variables = intent['variables'];
+    var len = list_variables.length;
+
+    while (len--) {
+        if (list_variables[len].entity_name == curr_entity) {
+            for (var i in list_variables[len].prompts) {
+                var value = list_variables[len].prompts[i];
+                var parent = document.getElementById('prompts-list');
+                document.getElementById('input-prompt').value = '';
+                createNewPromptRow(value, parent);
+            }
+        }
+    }
+}
+
+function cleanupromptDialogbox() {
+    var node = document.getElementById('prompts-list');
+    while (node.firstChild) node.removeChild(node.firstChild);
+}
+
+
+$(document).ready(function () {
+    loadVariablesFromIntent();
+    loadEntities();
+});
 
