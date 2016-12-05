a778aa30bb07dd6e60280b715eafbaf9ff3b3344
Hutoma <acibelli@hutoma.com>
2016-12-05 19:53:01 +0100

    UI - Remove unuseful code

--- Front-end/console/dynamic/chat.html.php
+++ Front-end/console/dynamic/chat.html.php
@@ -25,29 +25,6 @@
         </div>
     </div>
 </ul>
-<!--
-<div class="box-header no-border text-gray" style="background: #2e3032">
-    <i class="fa fa-user text-olive"></i>
-    <div class="box-title text-center" ><b><?php echo $_SESSION[ $_SESSION['navigation_id'] ]['user_details']['ai']['name']; ?></b></div>
-
-    <div class="pull-right">
-        <a href="#" class="dropdown-toggle" data-toggle="dropdown" data-toggle="tooltip" title="voice options" tabindex="-1" >
-            <i class="fa fa-gears text-gray"></i>
-        </a>
-
-        <ul class="dropdown-menu no-border flat">
-            <li id="speech-option" onMouseOver="this.style.cursor='pointer'">
-                <a id="speech-type"><i id="speech-icon" class="fa fa-microphone text-white"></i><spam id="speech-text" class="text-white"> Turn Off Speech</spam></a>
-            </li>
-
-            <li class="footer" id="json-option" value ="0" onClick="jsonOption(this.value)" onMouseOver="this.style.cursor='pointer'">
-                <a id="json-type"><i id="json-icon" class="fa fa-file-code-o text-white"></i><spam id="json-text" class="text-white"> Show Json Message</spam></a>
-            </li>
-
-        </ul>
-    </div>
-</div>
--->
 
 <div class="box box-solid flat no-shadow direct-chat-success no-margin"  style="background: #212121;">
     <div class="box-body flat" id="bodyChat"  style="margin-top: 1px">
