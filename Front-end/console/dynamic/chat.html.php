251453933c4339da2eb40bd6e5938423537df3b4
Hutoma <acibelli@hutoma.com>
2016-12-06 09:54:37 +0100

    UI - Missung User first_name in chat

--- Front-end/console/dynamic/chat.html.php
+++ Front-end/console/dynamic/chat.html.php
@@ -5,8 +5,8 @@
     }
 ?>
 <script>
-    var human =  'human';
-    var AI = <?php echo json_encode( $_SESSION[ $_SESSION['navigation_id'] ]['user_details']['ai']['name'] ); ?>
+    var human =  <?php echo json_encode( $_SESSION[$_SESSION['navigation_id']]['user_details']['first_name'] ); ?>;
+    var AI = <?php echo json_encode( $_SESSION[ $_SESSION['navigation_id'] ]['user_details']['ai']['name'] ); ?>;
 </script>
 <ul class="sidebar-menu" id="console-menu">
     <div class="box-header no-border text-gray text-center" style="background:#1e282c; color:#8A8A8A; height:35px; padding-top:10px;">
