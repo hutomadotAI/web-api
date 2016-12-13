4c66c6b6d4e9bca065ca1bd6539e22d64175c1f9
Hutoma <acibelli@hutoma.com>
2016-12-13 22:11:16 +0100

    UI - Botstore - Add Payment Modal Form

--- Front-end/console/dynamic/botstore.content.singleBot.buy.html.php
+++ Front-end/console/dynamic/botstore.content.singleBot.buy.html.php
@@ -1,32 +1,77 @@
-<!-- Modal BUY a Bot-->
-
 <div class="modal fade" id="buyBot" role="dialog">
     <div class="modal-dialog flat">
         <!-- Modal content-->
-        <div class="modal-content no-border no-shadow" style="background-color: transparent;">
-            <div class="modal-header">
-                <div class="col-xs-4 no-padding <?php echo $bot['widgetColor'];?> ">
-                    <div class="bot-icon" id="botIcon">
-                        <i class="<?php echo $bot['iconPath'];?>" style="padding-top:45px;"></i>
+        <div class="modal-content no-border bot-trasparent" style="background-color: transparent;" >
+
+            <div class="modal-body no-padding no-shadow no-border" >
+                <div class="box-body" style="min-width: 500px;">
+
+                    <div class="row no-margin">
+                            <div class="col-xs-1 no-padding">
+                                <div class="row no-margin">
+                                    <div class="col-xs-12 bot-trasparent bot-20">
+                                    </div>
+                                </div>
+                                <div class="row no-margin">
+                                    <div class="col-xs-12 bot-130" style="background:#212121;">
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-xs-3 no-padding">
+                                <div class="row no-margin">
+                                    <div class="col-xs-12 bot-150 <?php echo $bot['widgetColor'];?> text-bg">
+                                            <i class="<?php echo $bot['iconPath'];?>"></i>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-xs-8 no-padding">
+                                <div class="row no-margin">
+                                    <div class="col-xs-12 bot-trasparent bot-20">
+                                    </div>
+                                </div>
+                                <div class="row no-margin">
+                                    <div class="col-xs-12 bot-50" style="background:#212121;">
+                                        <div class="col-xs-8 bot-buy-title">
+                                            <?php echo $bot['name'];?>
+                                        </div>
+                                        <div class="col-xs-4 bot-buy-price" id="botTitle">   <!--title-->
+
+                                            <div class="pull-right text-orange text-md"> price
+                                                <?php echo number_format($bot['licenceFee'], 2, '.', ''); ?> £
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row no-margin">
+                                    <div class="col-xs-12 bot-80" style="background:#212121;padding-top:10px;">
+                                        <div class="text-md"><i class="fa fa-credit-card "></i> Add credit o debit card</div>
+                                        <div class="text-md"><i class="fa fa-cc-paypal"></i> Add PayPal</div>
+                                        <div class="text-md"><i class="fa  fa-gift"></i> Add promotional code</div>
+                                    </div>
+                                </div>
+                            </div>
                     </div>
-                </div>
-            </div>
 
-
-            <div class="modal-body" style="background-color: #212121" >
-                <div class="box-body" id="delete-ai-label">
+                    <div class="row no-margin" style="min-height:50px;background:#212121;">
+                    </div>
 
                 </div>
             </div>
-            <div class="modal-footer">
-                <form method="POST" id="deleteForm" action="">
-                    <button type="button" class="btn btn-primary flat" id="btnModelCancel" data-dismiss="modal">Cancel
-                    </button>
-                    <button type="submit" class="btn btn-danger flat" id="modalDelete" data-dismiss="modal">Delete
-                    </button>
-                </form>
-            </div>
+
         </div>
 
     </div>
+</div>
+
+
+<div class="modal-body" >
+    <div class="col-xs-6 bot-icon" style="background:red;">
+        ciao
+    </div>
+    <div class="col-xs-6" style="background:yellow;">
+        ciao
+    </div>
+
 </div>
