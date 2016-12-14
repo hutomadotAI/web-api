7b401fb57f4f61542ec4ae7f139d5e12bc306410
Hutoma <acibelli@hutoma.com>
2016-12-14 13:16:50 +0100

    UI - Botstore - Review Payment Modal Form position

--- Front-end/console/dynamic/botstore.content.singleBot.buy.html.php
+++ Front-end/console/dynamic/botstore.content.singleBot.buy.html.php
@@ -4,10 +4,13 @@
         <div class="modal-content bot-shadow" >
 
             <div class="modal-body no-padding no-shadow no-border" >
-                <div class="box-body bot-payment">
+                <div class="box-body bot-payment flat">
                     <div class="row no-margin">
                         <div class="col-xs-4 no-padding">
                             <div class="bot-icon-payment bot-absolute bot-shadow-light <?php echo $bot['widgetColor'];?> text-bg">
+                                <div class=".bot-icon-payment">
+                                    <i class="<?php echo $bot['iconPath'];?> text-white"></i>
+                                </div>
                             </div>
                         </div>
                         <div class="col-xs-8 no-padding">
@@ -18,7 +21,6 @@
                                         <?php echo $bot['name'];?>
                                     </div>
                                     <div class="col-xs-4 bot-buy-price" id="botTitle">
-
                                         <div class="pull-right text-orange text-md"> price
                                             <?php echo number_format($bot['licenceFee'], 2, '.', ''); ?> £
                                         </div>
@@ -26,12 +28,12 @@
                                 </div>
                             </div>
                             <div class="row no-margin">
-                                <div class="col-xs-6 bot-80" style="background:#212121;padding-top:10px;">
+                                <div class="col-xs-6 bot-70" style="padding-top:20px;">
                                     <div class="text-sm"><i class="fa fa-credit-card "></i> Add credit o debit card</div>
                                     <div class="text-sm"><i class="fa fa-cc-paypal"></i> Add PayPal</div>
                                     <div class="text-sm"><i class="fa  fa-gift"></i> Add promotional code</div>
                                 </div>
-                                <div class="col-xs-6">
+                                <div class="col-xs-6 bot-70" style="padding-top:35px;">
                                     <button class="btn btn-success pull-right flat" id="btnBuyBot" data-toggle="modal" data-target="#buyBot"value="" alt="buy a bot">
                                         <b>Buy new Bot</b>
                                         <span class="fa fa-arrow-circle-right"></span>
