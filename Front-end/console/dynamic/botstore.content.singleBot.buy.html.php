a03c753d9d955034e37671bad868f908bb803100
Hutoma <acibelli@hutoma.com>
2016-12-14 11:42:04 +0100

    UI - Botstore - Add Payment Modal Form

--- Front-end/console/dynamic/botstore.content.singleBot.buy.html.php
+++ Front-end/console/dynamic/botstore.content.singleBot.buy.html.php
@@ -1,6 +1,61 @@
 <div class="modal fade" id="buyBot" role="dialog">
-    <div class="modal-dialog flat">
+    <div class="modal-dialog flat" style=">
         <!-- Modal content-->
+        <div class="modal-content bot-shadow" >
+
+            <div class="modal-body no-padding no-shadow no-border" >
+                <div class="box-body bot-payment">
+                    <div class="row no-margin">
+                        <div class="col-xs-4 no-padding">
+                            <div class="bot-icon-payment bot-absolute bot-shadow-light <?php echo $bot['widgetColor'];?> text-bg">
+                            </div>
+                        </div>
+                        <div class="col-xs-8 no-padding">
+
+                            <div class="row no-margin">
+                                <div class="col-xs-12 bot-50">
+                                    <div class="col-xs-8 bot-buy-title">
+                                        <?php echo $bot['name'];?>
+                                    </div>
+                                    <div class="col-xs-4 bot-buy-price" id="botTitle">
+
+                                        <div class="pull-right text-orange text-md"> price
+                                            <?php echo number_format($bot['licenceFee'], 2, '.', ''); ?> £
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row no-margin">
+                                <div class="col-xs-6 bot-80" style="background:#212121;padding-top:10px;">
+                                    <div class="text-sm"><i class="fa fa-credit-card "></i> Add credit o debit card</div>
+                                    <div class="text-sm"><i class="fa fa-cc-paypal"></i> Add PayPal</div>
+                                    <div class="text-sm"><i class="fa  fa-gift"></i> Add promotional code</div>
+                                </div>
+                                <div class="col-xs-6" style="padding-top:50px;">
+                                    <button class="btn btn-success pull-right flat" id="btnBuyBot" data-toggle="modal" data-target="#buyBot"value="" alt="buy a bot">
+                                        <b>Buy new Bot</b>
+                                        <span class="fa fa-arrow-circle-right"></span>
+                                    </button>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
+
+
+
+
+<!--
+
+<div class="modal fade" id="buyBot" role="dialog">
+    <div class="modal-dialog flat">
+
         <div class="modal-content no-border bot-trasparent" style="background-color: transparent;" >
 
             <div class="modal-body no-padding no-shadow no-border" >
@@ -36,7 +91,7 @@
                                         <div class="col-xs-8 bot-buy-title">
                                             <?php echo $bot['name'];?>
                                         </div>
-                                        <div class="col-xs-4 bot-buy-price" id="botTitle">   <!--title-->
+                                        <div class="col-xs-4 bot-buy-price" id="botTitle">
 
                                             <div class="pull-right text-orange text-md"> price
                                                 <?php echo number_format($bot['licenceFee'], 2, '.', ''); ?> £
@@ -64,3 +119,5 @@
 
     </div>
 </div>
+
+-->
