e3607a778fab9c75111dc2cf54fe4ec81152c7bc
acibelli <acibelli@hutoma.com>
2017-01-25 09:20:22 +0000

    Merged PR 426: UI - Fixing bug 863 - 865
    UI - Fixing bug 863 - 865
    
    Related work items: #913, #906, #863, #865

--- web/src/pages/login.php
+++ web/src/pages/login.php
@@ -132,33 +132,7 @@ if(isset($_POST['action_login'])){
 </head>
 <body id="body">
 <?php include_once "../console/common/google_analytics.php"; ?>
-
-<header id="navigation" class="navbar-fixed-top navbar" >
-    <div class="container" style="font-weight: bold" >
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                <span class="sr-only">Toggle navigation</span>
-                <i class="fa fa-bars fa-2x"></i>
-            </button>
-            <a class="navbar-brand" href="#body">
-                <h1 style="padding: 5px;margin-left: 115px;" >
-                    <b> hu:toma </b>
-                </h1>
-            </a>
-        </div>
-        <nav style="padding: 5px;margin-right: 115px;" class="collapse navbar-collapse navbar-right" role="navigation">
-            <ul id="nav" class="nav navbar-nav">
-                <li><a href="https://hutoma.com" class="external">Home</a></li>
-                <li><a href="#" class="external">Login</a></li>
-                <li><a href="https://hutoma.com/index.html#features" class="external">Features  </a></li>
-                <li><a href="https://hutoma.com/about.html" class="external">About</a></li>
-                <li><a href="https://hutoma.com/contactus.html" class="external">Contacts</a></li>
-            </ul>
-        </nav>
-    </div>
-</header>
-
-
+<?php include_once "./header.php"; ?>
 
 <section>
     <div
@@ -207,56 +181,7 @@ if(isset($_POST['action_login'])){
     </div>
 </section>
 
+<?php include_once "./footer.php"; ?>
 
-<footer id="footer" class="footer">
-    <div class="container" style="font-weight: bold;">
-        <div class="row row-centered">
-
-            <div class="col-md-3 col-sm-3 col-s-3 col-centered wow fadeInUp animated" data-wow-duration="250ms"
-                 data-wow-delay="200ms">
-                <div class="footer-single">
-                    <h6>About</h6>
-                    <ul>
-                        <li><a class="af" href="https://hutoma.com/about.html">About Hutoma</a></li>
-                        <li><a class="af" href="https://hutoma.com/contactus.html">Contact Us</a></li>
-
-                    </ul>
-                </div>
-            </div>
-
-            <div class="col-md-3 col-sm-3 col-xs-3 col-centered wow fadeInUp animated" data-wow-duration="250ms"
-                 data-wow-delay="400ms">
-                <div class="footer-single">
-                    <h6>Get in Touch</h6>
-                    <ul>
-                        <li><a class="af"  href="https://twitter.com/hutomata"><i class="fa fa-twitter fa-lg"> </i> twitter</a></li>
-                        <li><a class="af" href="https://www.facebook.com/HutomadotAI/"><i class="fa fa-facebook fa-lg"></i>
-                                facebook</a></li>
-                        <li><a class="af" href="https://www.linkedin.com/company/hutoma"><i class="fa fa-linkedin fa-lg"></i>
-                                linkedin</a></li>
-                    </ul>
-
-                    </ul>
-                </div>
-            </div>
-
-            <div class="col-md-3 col-sm-3 col-xs-3 col-centered wow fadeInUp animated" data-wow-duration="250ms"
-                 data-wow-delay="500ms">
-                <div class="footer-single">
-                    <h6>Enterprise Customers</h6>
-                    <ul>
-                        <li><a class="af" href="mailto:hello@hutoma.com?subject=hutoma demo request">Schedule Demo</a></li>
-
-                    </ul>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-12">
-                    <p class="copyright text-center">
-                        Copyright © 2016 hu:toma</a>
-                    </p>
-                </div>
-            </div>
-</footer>
 </body>
 </html>
