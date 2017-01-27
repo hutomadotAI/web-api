aba4ef789f35e10e6d17a62b211410c4235f8484
acibelli <acibelli@hutoma.com>
2017-01-27 16:37:26 +0000

    Merged PR 472: UI - Unset sessions variables after delete AI
    UI - Unset sessions variables after delete AI
    
    Related work items: #950

--- web/src/console/dynamic/deleteai.php
+++ web/src/console/dynamic/deleteai.php
@@ -15,29 +15,8 @@ if (isset($_SESSION[$_SESSION['navigation_id']]['user_details']['ai']['aiid'])) 
     unset($aiApi);
 
     if ($response['status']['code'] === 200) {
-
-        unset($_SESSION[$_SESSION['navigation_id']]['user_details']['ai']['name']);
-        unset($_SESSION[$_SESSION['navigation_id']]['user_details']['ai']['description']);
-        unset($_SESSION[$_SESSION['navigation_id']]['user_details']['ai']['language']);
-        unset($_SESSION[$_SESSION['navigation_id']]['user_details']['ai']['timezone']);
-        unset($_SESSION[$_SESSION['navigation_id']]['user_details']['ai']['confidence']);
-        unset($_SESSION[$_SESSION['navigation_id']]['user_details']['ai']['personality']);
-        unset($_SESSION[$_SESSION['navigation_id']]['user_details']['ai']['voice']);
-        unset($_SESSION[$_SESSION['navigation_id']]['user_details']['ai']['userActivedDomains']);
-
-        unset($_SESSION[$_SESSION['navigation_id']]['user_details']['ai']['aiid']);
-        unset($_SESSION[$_SESSION['navigation_id']]['user_details']['ai']['created_on']);
-        unset($_SESSION[$_SESSION['navigation_id']]['user_details']['ai']['is_private']);
-        unset($_SESSION[$_SESSION['navigation_id']]['user_details']['ai']['deep_learning_error']);
-        unset($_SESSION[$_SESSION['navigation_id']]['user_details']['ai']['training_debug_info']);
-        unset($_SESSION[$_SESSION['navigation_id']]['user_details']['ai']['training_status']);
-        unset($_SESSION[$_SESSION['navigation_id']]['user_details']['ai']['status']);
-        //unset($_SESSION[ $_SESSION['navigation_id'] ]['user_details']['ai']['training_file']);
-
-        unset($_SESSION[$_SESSION['navigation_id']]['user_details']['ai']['contract']);
-        unset($_SESSION[$_SESSION['navigation_id']]['user_details']['ai']['payment_type']);
-        unset($_SESSION[$_SESSION['navigation_id']]['user_details']['ai']['price']);
-
+        unset($_SESSION[$_SESSION['navigation_id']]['user_details']['ai']);
+        unset($_SESSION[$_SESSION['navigation_id']]['user_details']['bot']);
     } else {
         unset($response);
         \hutoma\console::redirect('../error.php?err=203');
