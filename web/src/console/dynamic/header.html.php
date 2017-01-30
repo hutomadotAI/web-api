cc3f26e586a31c16ea98d4fb246dbc48d083914e
acibelli <acibelli@hutoma.com>
2017-01-30 08:53:01 +0000

    Merged PR 479: UI - add link for hu:toma logo
    UI - add link for hu:toma logo at top of web page
    
    Related work items: #841

--- web/src/console/dynamic/header.html.php
+++ web/src/console/dynamic/header.html.php
@@ -1,4 +1,4 @@
-<a href="http://www.hutoma.com" class="logo" tabindex="-1"><!-- mini logo for sidebar mini 50x50 pixels -->
+<a href="./home.php" class="logo" tabindex="-1"><!-- mini logo for sidebar mini 50x50 pixels -->
     <span class="logo-mini"><b>h</b>:</span>
     <!-- logo for regular state and mobile devices -->
     <span class="logo-lg"><b>hu:toma</b></span>
--- web/src/pages/header.php
+++ web/src/pages/header.php
@@ -5,7 +5,7 @@
                 <span class="sr-only">Toggle navigation</span>
                 <i class="fa fa-bars fa-2x"></i>
             </button>
-            <a class="navbar-brand" href="#body">
+            <a class="navbar-brand" href="./login.php">
                 <h1 style="padding: 5px;margin-left: 115px;" >
                     <b> hu:toma </b>
                 </h1>
