b468d69774788ee4b14bf013dd0e5ce07b43633a
acibelli <acibelli@hutoma.com>
2017-02-10 13:12:11 +0000

    Merged PR 565: UI - Integration refactor button coming soon and adjust style objects
    UI - Integration refactor to coming button soon and adjust style objects
    + set default values in sql to be zero ( not available )
    
    Related work items: #855

--- web/src/console/plugins/integration/integration.js
+++ web/src/console/plugins/integration/integration.js
@@ -4,28 +4,28 @@ function showIntegrations(str){
     for (var x in integrations) {
         if ( (str!=" ") && ( (str.length==0) || (integrations[x].name.toLowerCase()).indexOf(str.toLowerCase())!=-1 ) )  {
        
-            wHTML += ('<div class="box-body flat" style="background-color: #404446; border: 1px solid #202020;">');
+            wHTML += ('<div class="box-body flat" style="background-color: #404446; border: 1px solid #202020;padding: 5px 5px 5px 5px;">');
             wHTML += ('<div class="row">');
             wHTML += ('<div class="col-xs-1">');
             wHTML += ('<img src="./dist/img/social/icons/'+integrations[x].icon+'" width="30" height="30" alt="'+integrations[x].name+'">');
             wHTML += ('</div>');
-            wHTML += ('<div class="col-xs-3">');
+            wHTML += ('<div class="col-xs-4" style="padding-top:4px;">');
             if ( integrations[x].available != '0' )
-                wHTML += ('<label><span class="lead text">'+integrations[x].name+'</span></label>');
+                wHTML += ('<span class="text-md">'+integrations[x].name+'</span>');
             else
-                wHTML += ('<label><span class="lead text-muted">'+integrations[x].name+'</span></label>')
+                wHTML += ('<span class="text-md text-muted">'+integrations[x].name+'</span>')
             wHTML += ('</div>');
-            wHTML += ('<div class="col-xs-4">');
-            wHTML += ('<h5><span class="text pull-left">'+integrations[x].description+'</span></h5>');
+            wHTML += ('<div class="col-xs-4" style="padding-top:8px;">');
+            wHTML += ('<span class="text pull-left">'+integrations[x].description+'</span>');
             wHTML += ('</div>');
-            wHTML += ('<div class="col-xs-4">');
+            wHTML += ('<div class="col-xs-3">');
             if ( integrations[x].available != '0' ){
                 wHTML += ('<a data-toggle="collapse" data-parent="#accordion" href="#collapse'+(integrations[x].name).replace(" ","")+'">');
-                wHTML += ('<button class="btn btn-success pull-right text-sm flat" style="margin-right: 5px; margin-top: 3px; width:130px;"><i class="fa fa-download"></i> View more info</button>');
+                wHTML += ('<button class="btn btn-success pull-right text-sm flat no-margin" style="margin-right: 5px; margin-top: 3px; width:130px;"><i class="fa fa-download"></i> View more info</button>');
                 wHTML += ('</a>');
             }
             else
-                wHTML += ('<button class="btn btn-warning pull-right text-sm flat disabled" style="margin-right: 5px; margin-top: 3px; width:130px;"><i class="fa fa-exclamation-triangle"></i> Coming soon</button>');
+                wHTML += ('<button class="btn btn-primary pull-right text-sm text-white flat no-margin" style="margin-right: 5px; margin-top: 3px; width:130px;"><i class="fa fa-info-circle" style="padding-right:3px;"></i> Coming soon</button>');
 
             wHTML += ('</div>');
             wHTML += ('</div>');
@@ -34,7 +34,6 @@ function showIntegrations(str){
             wHTML += ('<div id="collapse'+(integrations[x].name).replace(" ","")+'" class="panel-collapse collapse">');
             wHTML += ('<div class="box-body ">');
             wHTML += ('<section class="invoice">');
-            wHTML += ('Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably have not heard of them accusamus labore sustainable VHS.');
             wHTML += ('</section>');
             wHTML += ('</div>');
             wHTML += ('</div>');
