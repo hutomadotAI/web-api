497e648d484c48643951446d485171a3776c8e65
acibelli <acibelli@hutoma.com>
2017-02-14 13:30:34 +0000

    Merged PR 583: UI - Add hutoma logo to header console pages
    UI - Add hutoma logo to header console pages
    
    Related work items: #1121

--- web/src/console/dynamic/header.html.php
+++ web/src/console/dynamic/header.html.php
@@ -1,8 +1,7 @@
 <a href="./home.php" class="logo" tabindex="-1"><!-- mini logo for sidebar mini 50x50 pixels -->
-    <span class="logo-mini"><b>h</b>:</span>
+    <span class="logo-mini"><img src="../console/dist/img/blueface.png"></span>
     <!-- logo for regular state and mobile devices -->
-    <span class="logo-lg"><b>hu:toma</b></span>
-
+    <span class="logo-lg"><img src="../console/dist/img/headerlogo.png"></span>
 </a>
 
 <nav class="navbar navbar-static-top"   role="navigation">
