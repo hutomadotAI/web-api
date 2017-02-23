07b0748208f1179f8521aa432e261bd2b8b0b2bc
David MG <mont@dixiak.com>
2017-02-23 17:26:47 +0000

    script to create test data and upload it to the API

---
+++ scripts/performance/performance_config.py
@@ -0,0 +1,35 @@
+class Config:
+    def __init__(self):
+        self.proxies = {
+            #'http': 'http://127.0.0.1:4444',
+        }
+        # self.urlRoot = "https://api.hutoma.com/v1"
+        self.url_root = "http://localhost:15000/v1"
+        #self.url_root = "http://10.150.0.4:8080/v1"
+
+        # dev token for donkey@hutoma.com (Load Testing account)
+        self.auth = 'eyJhbGciOiJIUzI1NiIsImNhbGciOiJERUYifQ.eNqqVgry93FVsgJT8W5Brq5KOkrFpUlAkSQTM6NEY5NE3ZTUZBNdk0TTJN0k4yQj3cTUREvDZMNU85QUA6VaAAAAAP__.c6zeCRsUV8Wd5X3ZjDPrHDoUoOzjnZZoWKy0tEY7rN4'
+
+        # test-role token for donkey@hutoma.com, used only for load-test chat endpoint
+        self.chat_auth = 'eyJhbGciOiJIUzI1NiIsImNhbGciOiJERUYifQ.eNqqVgry93FVsgJT8SGuwSFKOkrFpUlAkSQTM6NEY5NE3ZTUZBNdk0TTJN0k4yQj3cTUREvDZMNU85QUA6VaAAAAAP__.eUytifp7MPitydSm1sGQ8FVlp97CtgMvAlrt0AEyoRE'
+
+        # this is the list of training sizes for each target AI
+        self.training_sizes = [x for x in range(100, 1000, 200)]
+
+        # this is the list of (simultaneous requests, total requests) for each load test run
+        self.request_pattern = [(x, x * 5) for x in [1, 5, 10]]
+
+        self.common_words = [word.lower() for word in
+                             ['the', 'be', 'to', 'of', 'and', 'a', 'in', 'that', 'have', 'I', 'it', 'for',
+                              'not', 'on', 'with', 'he', 'as', 'you', 'do', 'at', 'this', 'but', 'his', 'by',
+                              'from', 'they', 'we', 'say', 'her', 'she', 'or', 'an', 'will', 'my', 'one', 'all',
+                              'would', 'there', 'their', 'what', 'so', 'up', 'out', 'if', 'about', 'who',
+                              'get', 'which', 'go', 'me', 'when', 'make', 'can', 'like', 'time', 'no', 'just',
+                              'him', 'know', 'take', 'people', 'into', 'year', 'your', 'good', 'some', 'could',
+                              'them', 'see', 'other', 'than', 'then', 'now', 'look', 'only', 'come', 'its',
+                              'over', 'think', 'also', 'back', 'after', 'use', 'two', 'how', 'our', 'work',
+                              'first', 'well', 'way', 'even', 'new', 'want', 'because', 'any', 'these',
+                              'give', 'most', 'us']]
+
+        self.unused_words_filename = "set_unused_words"
+        self.training_filename = "set_training"
---
+++ scripts/performance_create_test_data.py
@@ -0,0 +1,127 @@
+import random
+from pathlib import Path
+
+import hu_api.api
+from performance.common import get_ai_name, read_lines, write_file_lines, de_rate_limit
+from performance.performance_config import Config
+
+
+def create_sentence(source_word_set, common_words, used_words):
+    big_word_count = random.randint(1, 3)
+    small_word_count = 2 * big_word_count + random.randint(-1, 3)
+    using_big_words = random.sample(source_word_set, big_word_count)
+    used_words += set(using_big_words)
+    source_word_set -= set(using_big_words)
+    question = using_big_words + random.sample(common_words, small_word_count)
+    random.shuffle(question)
+    return ' '.join(question)
+
+
+def delete_load_test_ais():
+    for aiid in hu_api.api.find_ais(requester, "Load-Test"):
+        print('Deleting AI {0}'.format(aiid))
+        response = de_rate_limit(hu_api.api.delete_ai, requester, aiid)
+        if not response.success:
+            print('Failed to delete: {0}'.format(response.text))
+
+
+def create_upload_ai(size, filename):
+    name = get_ai_name(size)
+
+    print('Creating {0} AI '.format(name))
+    creation = de_rate_limit(hu_api.api.create_ai, requester, name, "Perf testing with {0} lines of training data".format(size))
+    if not creation.success:
+        print("Error creating AI: {0}".format(creation.text))
+    aiid = creation.response["aiid"]
+
+    print('Uploading new data file')
+    upload = de_rate_limit(hu_api.api.upload_training, requester, aiid, filename)
+    if not upload.success:
+        print("Error uploading training data: {0}".format(upload.text))
+
+    return aiid
+
+
+def train_start_ai(aiid):
+
+    print('Starting training')
+
+    while True:
+        status_request = de_rate_limit(hu_api.api.get_ai, requester, aiid)
+        if status_request.response["ai_status"] == "ai_ready_to_train":
+            break
+        else:
+            print("Waiting for status to flip ...")
+
+    train = de_rate_limit(hu_api.api.start_training, requester, aiid)
+    if not train.success:
+        print("Error starting training: {0}".format(train.text))
+
+
+def create_new_test_data():
+    # load the full word list
+    filtered = set([line.strip() for line in read_lines("wordlist.txt")
+                    if "'" not in line and line == line.lower()])
+
+    # remove common words
+    for word in config.common_words:
+        filtered.discard(word)
+
+    print('Unique words ', len(filtered))
+
+    used_question_words = []
+    used_answer_words = []
+    training_data = []
+
+    for a in range(config.training_sizes[-1] // 2):
+        question = create_sentence(filtered, config.common_words, used_question_words)
+        answer = create_sentence(filtered, config.common_words, used_answer_words)
+        training_data.append((question, answer))
+
+    print("Generated {0} pairs, using {1} unique Q words, {2} unique A words. {3} words left over."
+          .format(len(training_data), len(used_question_words), len(used_answer_words), len(filtered)))
+
+    write_file_lines(config.unused_words_filename, filtered)
+
+    for size in config.training_sizes:
+        text = []
+        for pair in training_data[:size // 2]:
+            text.append(pair[0])
+            text.append(pair[1])
+            text.append('')
+        filename = config.training_filename + "_" + str(size)
+        write_file_lines(filename, text)
+
+
+def create_and_train_new_ais():
+
+    aiids = []
+    for size in config.training_sizes:
+        filename = config.training_filename + "_" + str(size)
+        aiids.append(create_upload_ai(size, filename))
+
+    for aiid in aiids:
+        train_start_ai(aiid)
+
+
+def have_we_got_the_files():
+    missing_files = False
+    for size in config.training_sizes:
+        filename = config.training_filename + "_" + str(size)
+        if not Path(filename).is_file():
+            missing_files = True
+            print("Missing {0}; recreating all data files".format(filename))
+    return not missing_files
+
+config = Config()
+requester = hu_api.api.ApiRequester(config.url_root, config.auth, [])
+
+# if files are missing then recreate from scratch
+if not have_we_got_the_files():
+    create_new_test_data()
+
+# delete all the old load test AIs
+delete_load_test_ais()
+
+# upload new training data
+create_and_train_new_ais()
