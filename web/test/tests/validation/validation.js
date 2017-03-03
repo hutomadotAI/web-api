192092d50514ba0fa19afdc4086f7dd6616dbd77
Hutoma <acibelli@hutoma.com>
2017-03-03 11:48:50 +0100

    UI - According review

--- web/test/tests/validation/validation.js
+++ web/test/tests/validation/validation.js
@@ -17,37 +17,37 @@ QUnit.test("IsNameExists does not find entry", function (assert) {
 
 /** Test that URI item is allowed. **/
 
-QUnit.test("isInputInvalid allows entry", function (assert) {
+QUnit.test("URL with http and www is valid", function (assert) {
     var allowed = validation.isInputInvalid('http://www.hutoma.com','URI');
     assert.ok(allowed == false, "Passed");
 });
 
-QUnit.test("isInputInvalid allows entry", function (assert) {
+QUnit.test("URL with http and without www is valid", function (assert) {
     var allowed = validation.isInputInvalid('http://hutoma.com','URI');
     assert.ok(allowed == false, "Passed");
 });
 
-QUnit.test("isInputInvalid allows entry", function (assert) {
+QUnit.test("URL without http is valid", function (assert) {
     var allowed = validation.isInputInvalid('www.hutoma.com','URI');
     assert.ok(allowed == false, "Passed");
 });
 
-QUnit.test("isInputInvalid allows entry", function (assert) {
+QUnit.test("URL without http and www is valid", function (assert) {
     var allowed = validation.isInputInvalid('hutoma.com','URI');
     assert.ok(allowed == false, "Passed");
 });
 
-QUnit.test("isInputInvalid allows entry", function (assert) {
+QUnit.test("URL with http protocol is valid", function (assert) {
     var allowed = validation.isInputInvalid('https://www.hutoma.com','URI');
     assert.ok(allowed == false, "Passed");
 });
 
-QUnit.test("isInputInvalid allows entry", function (assert) {
+QUnit.test("URL with a path is valid", function (assert) {
     var allowed = validation.isInputInvalid('http://www.hutoma.com/pages','URI');
     assert.ok(allowed == false, "Passed");
 });
 
-QUnit.test("isInputInvalid allows entry", function (assert) {
+QUnit.test("URL with a path to file is valid", function (assert) {
     var allowed = validation.isInputInvalid('http://www.hutoma.com/pages/login.php','URI');
     assert.ok(allowed == false, "Passed");
 });
@@ -56,27 +56,22 @@ QUnit.test("isInputInvalid allows entry", function (assert) {
 
 /** Test that URI item is denied. **/
 
-QUnit.test("isInputInvalid denied entry", function (assert) {
+QUnit.test("URL empty is not valid", function (assert) {
     var allowed = validation.isInputInvalid('','URI');
     assert.ok(allowed == true, "Passed");
 });
 
-QUnit.test("isInputInvalid denied entry", function (assert) {
-    var allowed = validation.isInputInvalid(' ','URI');
-    assert.ok(allowed == true, "Passed");
-});
-
-QUnit.test("isInputInvalid denied entry", function (assert) {
+QUnit.test("URL composed by single word is not valid", function (assert) {
     var allowed = validation.isInputInvalid('hutoma','URI');
     assert.ok(allowed == true, "Passed");
 });
 
-QUnit.test("isInputInvalid denied entry", function (assert) {
+QUnit.test("URL without top-level domain is invalid", function (assert) {
     var allowed = validation.isInputInvalid('www.hutoma','URI');
     assert.ok(allowed == true, "Passed");
 });
 
-QUnit.test("isInputInvalid denied entry", function (assert) {
+QUnit.test("URL with different protocol by http and https is not valid", function (assert) {
     var allowed = validation.isInputInvalid('abc://www.hutoma.com','URI');
     assert.ok(allowed == true, "Passed");
 });
