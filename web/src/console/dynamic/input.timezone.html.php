bfcefad2d0e17b7f7a57e92d9808a89e6bc0b76e
Hutoma <acibelli@hutoma.com>
2017-05-11 15:08:22 +0200

    UI - Changes made according to the code review

--- web/src/console/dynamic/input.timezone.html.php
+++ web/src/console/dynamic/input.timezone.html.php
@@ -7,30 +7,30 @@ $html .= '<label for="ai_timezone">Time zone</label>';
 $html .= '<select class="form-control select2" name="ai_timezone" id="ai_timezone" style="width: 100%;">';
 
 foreach ($zones as $zone) {
-  $zone = explode('/', $zone); // 0 => Continent, 1 => City
-  // Only use "friendly" continent names
-  if ($zone[0] == 'Africa' ||
-      $zone[0] == 'America'||
-      $zone[0] == 'Antarctica' ||
-      $zone[0] == 'Arctic' ||
-      $zone[0] == 'Asia' ||
-      $zone[0] == 'Atlantic' ||
-      $zone[0] == 'Australia' ||
-      $zone[0] == 'Europe' ||
-      $zone[0] == 'Indian' ||
-      $zone[0] == 'Pacific')
+    $zone = explode('/', $zone); // 0 => Continent, 1 => City
+    // Only use "friendly" continent names
+    if ($zone[0] == 'Africa' ||
+        $zone[0] == 'America'||
+        $zone[0] == 'Antarctica' ||
+        $zone[0] == 'Arctic' ||
+        $zone[0] == 'Asia' ||
+        $zone[0] == 'Atlantic' ||
+        $zone[0] == 'Australia' ||
+        $zone[0] == 'Europe' ||
+        $zone[0] == 'Indian' ||
+        $zone[0] == 'Pacific')
 
-      if (isset($zone[1]) != '') {
-          $timezone = $zone[0] . '/' . $zone[1];
+        if (isset($zone[1]) && !empty($zone[1])){
+            $timezone = $zone[0] . '/' . $zone[1];
 
-          if (isset($zone[2]) != '')
-              $timezone .= '/' . $zone[2];
+            if (isset($zone[2]) && !empty($zone[2]))
+                $timezone .= '/' . $zone[2];
 
-          if ($timezone == 'Europe/London')
-            $html .= '<option selected="selected">'. $timezone .'</option>';
-          else
-            $html .= '<option>'. $timezone .'</option>';
-    }
+            if ($timezone == 'Europe/London')
+                $html .= '<option selected="selected">'. $timezone .'</option>';
+            else
+                $html .= '<option>'. $timezone .'</option>';
+        }
 }
 
 $html .= '</select></div>';
