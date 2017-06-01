3797c45d24be2857bfd3ef3a6621c177c3bb1284
Hutoma <acibelli@hutoma.com>
2017-06-01 12:30:06 +0200

    UI - Changes made according the code review comment romved also id to level. We do not need

--- web/src/console/scripts/sidebarMenu/sidebar.menu.js
+++ web/src/console/scripts/sidebarMenu/sidebar.menu.js
@@ -58,7 +58,7 @@ function buildConsoleMenu(ai_name, label_menu, level, block, limitedMenu) {
     wHTML += ('</ul>');
     wHTML += ('</li>');
 
-    wHTML += ('<li id="menu_documentation" class="unselectable"><a href="https://docs.hutoma.com" id="link_documentation" target="_blank"><i class="fa fa-book text-purple" id="level0"></i><span>Documentation</span></a></li>');
+    wHTML += ('<li id="menu_documentation" class="unselectable"><a href="https://docs.hutoma.com" id="link_documentation" target="_blank"><i class="fa fa-book text-purple"></i><span>Documentation</span></a></li>');
 
     newNode.innerHTML = wHTML;
     document.getElementById('sidebarmenu').appendChild(newNode);
