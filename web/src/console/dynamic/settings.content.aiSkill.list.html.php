eb2fd67cb5febe3e1f08fc67e7a3e22aeb509ab3
Hutoma <acibelli@hutoma.com>
2017-06-12 18:07:25 +0200

    UI - Make invisible hidden input on submit

--- web/src/console/dynamic/settings.content.aiSkill.list.html.php
+++ web/src/console/dynamic/settings.content.aiSkill.list.html.php
@@ -1,7 +1,7 @@
 <p></p>
 <h2></h2>
 <p id="botsSearch"></p>
-<form method="POST" name="aiSkill" >
-    <input type="hidden" name="userActivedBots" id="userActivedBots" value="">
+<form method="POST" name="aiSkill">
+    <input type="hidden" name="userActivedBots" id="userActivedBots" value="" style="display:none;">
 </form>
 
