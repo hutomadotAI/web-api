25b1db2e16d30c9bf6f0e5acf4c04352a64e9400
Hutoma <acibelli@hutoma.com>
2017-06-20 19:52:38 +0200

    UI - add unit test for webhook

--- web/test/tests/validation/webhook.js
+++ web/test/tests/validation/webhook.js
@@ -1,21 +1,21 @@
 /** Test that WEBHOOK URI item is allowed. **/
 
-QUnit.test("URL with http and www is valid", function (assert) {
+QUnit.test("WEBHOOK URL with http and www is valid", function (assert) {
     var allowed = validation.isInputInvalid('http://www.hutoma.com','webhook');
     assert.ok(allowed == false, "Passed");
 });
 
-QUnit.test("URL with http and without www is valid", function (assert) {
+QUnit.test("WEBHOOK URL with http and without www is valid", function (assert) {
     var allowed = validation.isInputInvalid('http://hutoma.com','webhook');
     assert.ok(allowed == false, "Passed");
 });
 
-QUnit.test("URL with http protocol is valid", function (assert) {
+QUnit.test("WEBHOOK URL with http protocol is valid", function (assert) {
     var allowed = validation.isInputInvalid('https://www.hutoma.com','webhook');
     assert.ok(allowed == false, "Passed");
 });
 
-QUnit.test("URL with a path is valid", function (assert) {
+QUnit.test("WEBHOOK URL with a path is valid", function (assert) {
     var allowed = validation.isInputInvalid('http://www.hutoma.com/pages','webhook');
     assert.ok(allowed == false, "Passed");
 });
@@ -29,32 +29,32 @@ QUnit.test("URL with a path to file is valid", function (assert) {
 
 /** Test that WEBHOOK URI item is denied. **/
 
-QUnit.test("URL empty is not valid", function (assert) {
+QUnit.test("WEBHOOK URL empty is not valid", function (assert) {
     var allowed = validation.isInputInvalid('','webhook');
     assert.ok(allowed == true, "Passed");
 });
 
-QUnit.test("URL composed by single word is not valid", function (assert) {
+QUnit.test("WEBHOOK URL composed by single word is not valid", function (assert) {
     var allowed = validation.isInputInvalid('hutoma','webhook');
     assert.ok(allowed == true, "Passed");
 });
 
-QUnit.test("URL without top-level domain is invalid", function (assert) {
+QUnit.test("WEBHOOK URL without top-level domain is invalid", function (assert) {
     var allowed = validation.isInputInvalid('www.hutoma','webhook');
     assert.ok(allowed == true, "Passed");
 });
 
-QUnit.test("URL without http is not valid", function (assert) {
+QUnit.test("WEBHOOK URL without http is not valid", function (assert) {
     var allowed = validation.isInputInvalid('www.hutoma.com','webhook');
     assert.ok(allowed == true, "Passed");
 });
 
-QUnit.test("URL without http and www is not valid", function (assert) {
+QUnit.test("WEBHOOK URL without http and www is not valid", function (assert) {
     var allowed = validation.isInputInvalid('hutoma.com','webhook');
     assert.ok(allowed == true, "Passed");
 });
 
-QUnit.test("URL with different protocol by http and https is not valid", function (assert) {
+QUnit.test("WEBHOOK URL with different protocol by http and https is not valid", function (assert) {
     var allowed = validation.isInputInvalid('abc://www.hutoma.com','webhook');
     assert.ok(allowed == true, "Passed");
 });
