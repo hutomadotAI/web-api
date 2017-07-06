16cdf5ad9d57229b31d6cceb7020312e50e8b764
Pedro Teixeira <pedrotei@hutoma.com>
2017-07-06 10:55:11 +0000

    Merged PR 1315: Fixed webhook url regex validation. It was expecting the 3rd part of an IP
    Fixed webhook url regex validation. It was expecting the 3rd part of an IP address to be alpha only, which is obviously incorrect. Also fixed the URI validation tahthad the same problem. This problem has been in the code for a while (exists in PROD) but I guess we haven't had validated IP addresses with that code yet
    
    Related work items: #2391

--- web/test/tests/validation/webhook.js
+++ web/test/tests/validation/webhook.js
@@ -32,6 +32,14 @@ QUnit.test("WEBHOOK URL with no .com domains", function (assert) {
     assert.notOk(validation.isInputInvalid('http://www.hutoma.co.uk','webhook'));
 });
 
+QUnit.test("WEBHOOK URL supports IP addresses", function (assert) {
+    assert.notOk(validation.isInputInvalid('http://192.168.0.1','webhook'));
+});
+
+QUnit.test("WEBHOOK URL supports URLs with port", function (assert) {
+    assert.notOk(validation.isInputInvalid('http://www.aaa.com:1234','webhook'));
+});
+
 /** Test that WEBHOOK URI item is denied. **/
 
 QUnit.test("WEBHOOK URL empty is not valid", function (assert) {
