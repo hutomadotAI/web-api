e1bfb9e709e303421fc85bdfc48cfb5a9e6ee48e
Paul Annetts <paul.annetts@hutoma.com>
2017-07-07 16:36:58 +0000

    Merged PR 1323: Bug 2395 - fix getAisLinkedToAi Stored Procedure
    Bug 2395 - fix getAisLinkedToAi Stored Procedure
    Must get devID from botStore table, not bot_ai.
    
    Related work items: #2395

--- db/structure.sql
+++ db/structure.sql
@@ -1977,10 +1977,10 @@ DELIMITER ;;
 CREATE DEFINER=`aiReader`@`127.0.0.1` PROCEDURE `getAisLinkedToAi`(
   IN `param_devId` VARCHAR(50),
   IN `param_aiid` VARCHAR(50))
-  BEGIN
-    SELECT bai.aiid as 'ai', bs.aiid as 'linked_ai', bai.dev_id as 'linked_ai_devId', ai.ui_ai_confidence as 'minP'
-    FROM bot_ai bai INNER JOIN botStore bs ON bs.id = bai.botId INNER JOIN ai ai ON ai.aiid = bs.aiid
-    WHERE bai.aiid=param_aiid AND bai.dev_id=param_devId;
+BEGIN
+	SELECT bai.aiid as 'ai', bs.aiid as 'linked_ai', bs.dev_id as 'linked_ai_devId', ai.ui_ai_confidence as 'minP'
+	FROM bot_ai bai INNER JOIN botStore bs ON bs.id = bai.botId INNER JOIN ai ai ON ai.aiid = bs.aiid
+	WHERE bai.aiid=param_aiid AND bai.dev_id=param_devId;
   END ;;
 DELIMITER ;
 /*!50003 SET sql_mode              = @saved_sql_mode */ ;
