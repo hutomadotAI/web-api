80cbc1e90100e699fb9f24e302d99eb00a07f5da
Pedro Teixeira <pedrotei@hutoma.com>
2017-07-13 09:48:31 +0000

    Merged PR 1344: (Cherry-Pick) Fix "purchasing" bot on the mini-card to open the store on the top window.
    Fix "purchasing" bot on the mini-card to open the store on the top window.
    Also fixed the "racing condition" where there were 2 competing redirects (js and href) when opening a bot card page.
    
    Cherry picked from Pull request 1338.
    
    Related work items: #2441

--- web/src/console/scripts/botcard/buyBot.js
+++ web/src/console/scripts/botcard/buyBot.js
@@ -34,7 +34,7 @@ function purchaseBot() {
                                 break;
                         }
                     } catch (err) {
-                        location.href = "/pages/login.php?redirect=/console/botstore.php?botId=" + botId;
+                        top.location.href = "/pages/login.php?redirect=/console/botstore.php?botId=" + botId;
                     }
                 },
                 complete: function () {
