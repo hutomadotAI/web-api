841a3361484f4b3343fcd5de8680b8a3c09bd556
maurizio cibelli <mcibelli@hotmail.com>
2017-08-01 06:42:37 +0200

    adding tag to track how many visiting users register

--- web/src/pages/register.php
+++ web/src/pages/register.php
@@ -235,6 +235,24 @@ if(isset($_POST['submit'])) {
 </section>
 
 <?php include_once "./footer.php"; ?>
+<!-- Google Code for sign-up Conversion Page -->
+<script type="text/javascript">
+    /* <![CDATA[ */
+    var google_conversion_id = 843069449;
+    var google_conversion_language = "en";
+    var google_conversion_format = "3";
+    var google_conversion_color = "ffffff";
+    var google_conversion_label = "LtRjCIPTx3MQifCAkgM";
+    var google_remarketing_only = false;
+    /* ]]> */
+</script>
+<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+</script>
+<noscript>
+    <div style="display:inline;">
+        <img height="1" width="1" style="border-style:none;" alt="" src="//www.googleadservices.com/pagead/conversion/843069449/?label=LtRjCIPTx3MQifCAkgM&amp;guid=ON&amp;script=0"/>
+    </div>
+</noscript>
 
 </body>
 </html>
