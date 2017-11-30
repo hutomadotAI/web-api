041b129ac3c979524375cb900a39f458706f67d6
David MG <david@hutoma.ai>
2017-11-30 14:42:16 +0000

    load limit testing script change

--- scripts/chat_test.py
+++ scripts/chat_test.py
@@ -1,5 +1,6 @@
 
 import random
+from queue import Queue
 from threading import Thread
 
 import time
@@ -9,6 +10,50 @@ from pathlib import Path
 import hu_api
 from performance.common import check_api_available, de_rate_limit, read_training_file, lines_to_words, find_ais, chat_ai
 from performance.performance_config import Config, make_config
+
+
+class Worker(Thread):
+    """ Thread executing tasks from a given tasks queue """
+    def __init__(self, args, results):
+        Thread.__init__(self)
+        self.args = args
+        self.execute = True
+        self.results = results
+
+    def run(self):
+        while self.execute:
+            (botlist, requester) = self.args
+
+            name = random.sample(botlist.keys(), 1)[0]
+            (aiid, words) = botlist[name]
+
+            start_time = time.time()
+            success = False
+            result = ''
+
+            try:
+                question = '{} {}'.format(
+                    random.sample(words, 1)[0],
+                    random.sample(words, 1)[0])
+
+                chat = de_rate_limit(hu_api.api.chat, requester, aiid, question)
+                if chat and chat.response and chat.response['status'] and chat.response['status']['code']:
+                    code = chat.response['status']['code']
+                    if code != 200:
+                        result = "{} ERR {}".format(name, code)
+                    else:
+                        result = "{} OK Q\"{}\" A\"{}\"".format(name, question, chat.response['result']['answer'])
+                        success = True
+                else:
+                    result = "{} ERROR {}".format(name, chat.status_code)
+            except Exception as e:
+                result = str(e)
+            finally:
+                duration = time.time() - start_time
+                self.results.put((success, duration, result))
+
+    def stop(self):
+        self.execute = False
 
 
 def main():
@@ -21,49 +66,41 @@ def main():
 
     botlist = find_ais(requester, chat_test_defs)
 
-    requests_per_bot = 100
-    max_simultaneous = 100
+    if not botlist:
+        print("No data")
+        exit(0)
 
     non_words = {'xyzzy', 'qwklpqlwkqq', 'mnbdqwtplqww'}
     aiml_bot_id = '40bf861c-7289-4c9b-960c-5b917879cb43'
     server = config.server
 
+    if server == 'rnn':
+        botlist = {name: (aiid, non_words) for (name, (aiid, words)) in botlist}
+    if server == 'aiml':
+        botlist = {'aiml': (aiml_bot_id, non_words) for (name, (aiid, words)) in botlist}
+
+    threads = []
+    load = 1.0
+    result_queue = Queue()
+
     while True:
-        started_at = time.time()
+        need_active = int(round(load))
+        while need_active > len(threads):
 
-        # ten seconds per round
-        next_one_after = started_at + 10
+            threaded_requester = hu_api.api.ApiRequester(config.url_root, config.auth, [])
+            thread = Worker(args=(botlist, threaded_requester), results=result_queue)
+            threads.append(thread)
+            thread.start()
 
-        threads = []
-        for name in sorted(botlist.keys()):
-            (aiid, words) = botlist[name]
+        while need_active < len(threads):
+            threads.pop().stop()
 
-            if server == 'rnn':
-                words = non_words
-            if server == 'aiml':
-                aiid = aiml_bot_id
-                requests_per_bot = max_simultaneous
+        while not result_queue.empty():
+            (success, duration, result) = result_queue.get(False)
+            success_message = "OK" if success else "ERROR"
+            print("{} {} {}".format(success_message, duration, result))
 
-            for request_id in range(0, requests_per_bot):
-
-                # only the first 10 bots so as not to thrash bot-data
-                if len(threads) < max_simultaneous:
-                    threaded_requester = hu_api.api.ApiRequester(config.url_root, config.auth, [])
-                    thread = Thread(target=chat_ai, args=(aiid, name, words, threaded_requester))
-                    threads.append(thread)
-                    thread.start()
-
-        for thread_join in threads:
-            thread_join.join()
-
-        time_now = time.time()
-        time_to_wait = next_one_after - time_now
-
-        if time_to_wait > 0:
-            print('Waiting {} seconds for the next round ...'.format(time_to_wait))
-            time.sleep(time_to_wait)
-        else:
-            print('Round is {} seconds late ..... starting now'.format(0 - time_to_wait))
+        time.sleep(0.5)
 
 
 if __name__ == "__main__":
