970753bc52fa45e18cdea7121ec6e5e4bc014e46
Paul Annetts <paul@hutoma.ai>
2018-06-08 12:33:56 +0100

    Add update-training to API CLI Checked that update-training also does an automatic retrain.

--- scripts/api_cli.py
+++ scripts/api_cli.py
@@ -93,6 +93,10 @@ def main(args, parser):
         elif command == "delete-all-ais":
             print("Delete all AIs")
             hu_api.api.delete_all_ais(requester)
+        elif command == "update-training":
+            ai_id = args.ai_id
+            print("Update training")
+            hu_api.api.update_training(requester, ai_id)
         else:
             arg_error(parser, "command '{}' is not recognized".format(command))
 
@@ -132,6 +136,9 @@ if __name__ == "__main__":
     parser_chat = subparsers.add_parser('chat', help='Chat with an AI')
     parser_chat.add_argument('ai_id', help='AI ID')
     parser_chat.add_argument('chat_input', help='Chat input')
+    # update training
+    parser_update_training = subparsers.add_parser('update-training', help='Update training - causes chat-cores to be retrained')
+    parser_update_training.add_argument('ai_id', help='AI ID')
     # delete AI
     parser_delete = subparsers.add_parser('delete-ai', help='Delete an AI')
     parser_delete.add_argument('ai_id', help='AI ID')
