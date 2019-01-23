88b07a4c7384dd8cd2ab3cc3c4623c05ea0ed6ca
Paul Annetts <paul.annetts@hutoma.com>
2019-01-23 10:26:21 +0000

    Merged PR 3804: Add feature toggling to language support
    Add feature toggling to language support
    Only EN is generally available for now.
    All other languages will require a toggle to be set.
    
    Related work items: #6737

--- service/core-service/src/main/java/com/hutoma/api/logic/LanguageLogic.java
+++ service/core-service/src/main/java/com/hutoma/api/logic/LanguageLogic.java
@@ -3,8 +3,13 @@ package com.hutoma.api.logic;
 import com.hutoma.api.common.Config;
 import com.hutoma.api.common.FeatureToggler;
 import com.hutoma.api.common.SupportedLanguage;
+import com.hutoma.api.common.FeatureToggler.FeatureState;
+import com.hutoma.api.logging.ILogger;
+import com.hutoma.api.logging.LogMap;
 
 import java.util.stream.Collectors;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
 import java.util.Optional;
@@ -18,12 +23,17 @@ import javax.inject.Inject;
  */
 public class LanguageLogic {
     private static final String LOGFROM = "languagelogic";
+    private static final Set<SupportedLanguage> GENERAL_AVAILABILITY_LANGUAGES = new HashSet<>(
+            Arrays.asList(SupportedLanguage.EN));
+    
+    private final ILogger logger;
     private final Config config;
     private final FeatureToggler featureToggler;
-    private Set<SupportedLanguage> availableLanguages;
+    private Set<SupportedLanguage> systemAvailableLanguages;
 
     @Inject
-    public LanguageLogic(final Config config, final FeatureToggler featureToggler) {
+    public LanguageLogic(final ILogger logger, final Config config, final FeatureToggler featureToggler) {
+        this.logger = logger;
         this.config = config;
         this.featureToggler = featureToggler;
     }
@@ -36,7 +46,7 @@ public class LanguageLogic {
     }
 
     public Optional<SupportedLanguage> getAvailableLanguage(final String langCode, final UUID devId, final UUID aiid) {
-        initAvailableLanguages();
+        initSystemAvailableLanguages();
         if (langCode == null || langCode.isEmpty()) {
             return Optional.empty();
         }
@@ -46,13 +56,38 @@ public class LanguageLogic {
             return Optional.empty();
         }
         SupportedLanguage supportedLanguage = supportedLanguageOpt.get();
-        return availableLanguages.contains(supportedLanguage) ? supportedLanguageOpt : Optional.empty();
+        if (!systemAvailableLanguages.contains(supportedLanguage)) {
+            return Optional.empty();
+        }
+        if (GENERAL_AVAILABILITY_LANGUAGES.contains(supportedLanguage)) {
+            return supportedLanguageOpt;
+        }
+                
+        String devIdString = devId != null ? devId.toString() : "null";
+        String languageName = supportedLanguage.toString().toLowerCase();
+        LogMap logMap = LogMap.map("Language", languageName)
+                              .put("aiid", aiid != null ? aiid.toString() : "null");
+        // this language is on the system, but not GA. Check the feature toggle
+        String featureName = String.format("language-%s", languageName);
+        FeatureState featureState = featureToggler.getStateForAiid(devId, aiid, featureName);
+        switch (featureState) {
+            case C:
+                this.logger.logUserWarnEvent(LOGFROM, "Cannot use language: " + languageName,
+                    devIdString, logMap);
+                return Optional.empty();
+            case T1:
+                return supportedLanguageOpt;
+            default:
+                this.logger.logUserErrorEvent(LOGFROM, "Invalid toggle state " + languageName,
+                    devIdString, logMap.put("toggleState", featureState.toString()));
+                return Optional.empty();
+        }
     }
 
-    private void initAvailableLanguages() {
-        if (availableLanguages == null) {
+    private void initSystemAvailableLanguages() {
+        if (systemAvailableLanguages == null) {
             List<String> langs = config.getLanguagesAvailable();
-            availableLanguages = langs.stream()
+            systemAvailableLanguages = langs.stream()
                 .map(SupportedLanguage::get)
                 .filter(Optional::isPresent)
                 .map(Optional::get)
--- service/core-service/src/test/java/com/hutoma/api/logic/TestLanguageLogic.java
+++ service/core-service/src/test/java/com/hutoma/api/logic/TestLanguageLogic.java
@@ -1,6 +1,8 @@
 package com.hutoma.api.logic;
 
 import com.hutoma.api.common.*;
+import com.hutoma.api.common.FeatureToggler.FeatureState;
+import com.hutoma.api.logging.ILogger;
 
 import java.util.Arrays;
 import java.util.Locale;
@@ -21,6 +23,7 @@ public class TestLanguageLogic {
 
     private LanguageLogic languageLogic;
 
+    private ILogger fakeLogger;
     private Config fakeConfig;
     private FeatureToggler fakeFeatureToggler;
 
@@ -29,11 +32,14 @@ public class TestLanguageLogic {
 
     @Before
     public void setup() {
+        this.fakeLogger = mock(ILogger.class);
         this.fakeConfig = mock(Config.class);
         this.fakeFeatureToggler = mock(FeatureToggler.class);
 
         when(this.fakeConfig.getLanguagesAvailable()).thenReturn(Arrays.asList("en", "es"));
-        this.languageLogic = new LanguageLogic(this.fakeConfig,
+        when(this.fakeFeatureToggler.getStateForAiid(any(), any(), any())).thenReturn(FeatureState.C);
+
+        this.languageLogic = new LanguageLogic(this.fakeLogger, this.fakeConfig,
                 this.fakeFeatureToggler);
     }
 
@@ -71,6 +77,40 @@ public class TestLanguageLogic {
     public void testFrLanguageInvalid() {
         Optional<SupportedLanguage> result = this.languageLogic.getAvailableLanguage("fr", null, null);
         Assert.assertEquals(Optional.empty(), result);
+    }
+
+    @Test
+    public void testEsLocaleInvalidUntoggled() {
+        Optional<SupportedLanguage> result = this.languageLogic.getAvailableLanguage(new Locale("es", "ES"), null, null);
+        Assert.assertEquals(Optional.empty(), result);
+    }
+
+    @Test
+    public void testEsLanguageInvalidUntoggled() {
+        Optional<SupportedLanguage> result = this.languageLogic.getAvailableLanguage("es", null, null);
+        Assert.assertEquals(Optional.empty(), result);
+    }
+
+    @Test
+    public void testEsLocaleValidToggled() {
+        when(this.fakeFeatureToggler.getStateForAiid(any(), any(), any())).thenReturn(FeatureState.T1);
+        Optional<SupportedLanguage> result = this.languageLogic.getAvailableLanguage(new Locale("es", "ES"), null, null);
+        Assert.assertEquals(Optional.of(SupportedLanguage.ES), result);
+    }
+
+    @Test
+    public void testEsLanguageValidToggled() {
+        when(this.fakeFeatureToggler.getStateForAiid(any(), any(), any())).thenReturn(FeatureState.T1);
+        Optional<SupportedLanguage> result = this.languageLogic.getAvailableLanguage("es", null, null);
+        Assert.assertEquals(Optional.of(SupportedLanguage.ES), result);
+    }
+
+    @Test
+    public void testEsLanguageInvalidBadToggle() {
+        when(this.fakeFeatureToggler.getStateForAiid(any(), any(), any())).thenReturn(FeatureState.T2);
+        Optional<SupportedLanguage> result = this.languageLogic.getAvailableLanguage("es", null, null);
+        Assert.assertEquals(Optional.empty(), result);
+        verify(fakeLogger, times(1)).logUserErrorEvent(any(), any(), any(), any());
     }
 }
 
